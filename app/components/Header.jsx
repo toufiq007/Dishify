3c0bea942579b8ad9b927f209a14be1a5df581c5
toufiq007 <t.limon2342@gmail.com>
2024-12-01 17:45:18 +0600

    created our header section

---
+++ app/components/Header.jsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <>
+      <header className="container mx-auto px-4 py-4 shadow-lg fixed top-0 bg-white z-50">
+        <nav className="flex justify-between items-center">
+          <a href="/" className="text-3xl font-bold">
+            <Image
+              src="/assets/lws-kitchen.png"
+              className="h-10"
+              alt="logo"
+              width={100}
+              height={100}
+            />
+          </a>
+          <ul className="hidden md:flex space-x-6">
+            <li>
+              <Link href="/" className="hover:text-orange-500">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/categorires" className="hover:text-orange-500">
+                Categories
+              </Link>
+            </li>
+            <li>
+              <Link href="/recipies" className="hover:text-orange-500">
+                Latest Recipes
+              </Link>
+            </li>
+          </ul>
+          <div className="flex items-center space-x-4">
+            <Link href="#" className="hover:text-orange-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,9 +21,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="bg-white">
+        <Header />
         {children}
       </body>
     </html>
