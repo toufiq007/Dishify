ff31e62a5153e1989b9fbddf3aa87750d9063c6d
toufiq007 <t.limon2342@gmail.com>
2024-12-01 23:00:29 +0600

    navigate successfully by banner details page

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,28 +1,45 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import allRecipes from "../../public/data/recipes.json";
+import allCategories from "../../public/data/categories.json";
+  
 const Banner = () => {
+  const allRecipesWithCategoryName = allRecipes.map((recipe) => {
+    const category = allCategories.find(
+      (category) => category.id === recipe.category_id
+    );
+    if (category) {
+      return { ...recipe, categoryName: category.name };
+    }
+    return recipe;
+  });
+
+  console.log(allRecipesWithCategoryName, "with category name");
+
   return (
     <>
       <section className="mb-16 bg-orange-50">
         <div className="grid md:grid-cols-2 gap-8 items-center">
-          <div>
-            <Image
-              src="/assets/tir.jpg"
-              width={500}
-              height={200}
-              alt="Mighty Super Cheesecake"
-              className="w-full h-[450px] object-cover rounded-lg"
-            />
-          </div>
+          <Link
+            href={`/${allRecipesWithCategoryName[0]?.categoryName}/${allRecipesWithCategoryName[0]?.title}`}
+          >
+            <div>
+              <Image
+                src={`/assets/thumbs/${allRecipesWithCategoryName[0]?.thumbnail}`}
+                width={500}
+                height={200}
+                alt="Mighty Super Cheesecake"
+                className="w-full h-[450px] object-cover rounded-lg"
+              />
+            </div>
+          </Link>
           <div>
             <h1 className="text-4xl font-bold mb-4">
-              Decadent Tiramisu Delight
+              {allRecipesWithCategoryName[0]?.title}
             </h1>
             <p className="text-gray-600 mb-4">
-              Indulge in the ultimate Italian dessert experience with our
-              velvety smooth tiramisu. Layers of coffee-soaked ladyfingers and
-              creamy mascarpone filling create a heavenly treat that's sure to
-              impress.
+              {allRecipesWithCategoryName[0]?.description}
             </p>
             <Link
               href="/blog-details.html"
