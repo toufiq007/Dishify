44494a5280f55a1b1b386dd6f4fcab978524b1a5
toufiq007 <t.limon2342@gmail.com>
2024-12-01 17:32:12 +0600

    initial setup for app

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h2>hello dilishy</h2>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Playfair Display", "serif"],
+      },
     },
   },
   plugins: [],
