1d8bec8d12b6267697ee07b6d7acf22e6146737f
toufiq007 <t.limon2342@gmail.com>
2024-12-01 20:10:18 +0600

    add dynamic data in the latestRecipes component with sort by the date and add categoryName in the recipes data

--- app/components/LatestRecipes.jsx
+++ app/components/LatestRecipes.jsx
@@ -1,55 +1,42 @@
 import Image from "next/image";
+import allRecipes from "../../public/data/recipes.json";
+import allCategories from "../../public/data/categories.json";
 
 const LatestRecipes = () => {
+  // add categoryName in the recipes array by finding by the category id
+  const modifiyArray = allRecipes.map((recipe) => {
+    const category = allCategories.find(
+      (category) => category.id === recipe.category_id
+    );
+    if (category) {
+      return { ...recipe, categoryName: category.name };
+    }
+    return recipe;
+  });
+
+  // sort the recipe array by date
+  const sortedRecipes = modifiyArray.sort(
+    (a, b) => new Date(b.published_date) - new Date(a.published_date)
+  );
+
   return (
     <>
       <section className="mb-16">
         <h2 className="text-3xl font-bold mb-8">Latest Recipes</h2>
         <div className="grid md:grid-cols-4 gap-8">
-          <div>
-            <Image
-              width={400}
-              height={100}
-              src="https://images.unsplash.com/photo-1559564121-d12c8f29e8cd?q=100&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Strawberry Cream"
-              className="w-full h-[300px] object-cover rounded-lg mb-4"
-            />
-            <h3 className="text-lg font-semibold mb-2">Strawberry Cream</h3>
-            <p className="text-gray-600">Dessert</p>
-          </div>
-          <div>
-            <Image
-              width={400}
-              height={100}
-              src="https://plus.unsplash.com/premium_photo-1664472634106-1430c3973e68?q=100&w=1674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Creamy Garlic Pasta"
-              className="w-full h-[300px] object-cover rounded-lg mb-4"
-            />
-            <h3 className="text-lg font-semibold mb-2">Creamy Garlic Pasta</h3>
-            <p className="text-gray-600">Pasta</p>
-          </div>
-          <div>
-            <Image
-              width={400}
-              height={100}
-              src="https://images.unsplash.com/photo-1694067508035-6cccccb95cb0?q=100&w=1674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Blueberry Muffins"
-              className="w-full h-[300px] object-cover rounded-lg mb-4"
-            />
-            <h3 className="text-lg font-semibold mb-2">Blueberry Muffins</h3>
-            <p className="text-gray-600">Breakfast</p>
-          </div>
-          <div>
-            <Image
-              width={400}
-              height={100}
-              src="https://images.unsplash.com/photo-1565557623262-b51c2513a641?q=100&w=1671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Chicken Tikka Masala"
-              className="w-full h-[300px] object-cover rounded-lg mb-4"
-            />
-            <h3 className="text-lg font-semibold mb-2">Chicken Tikka Masala</h3>
-            <p className="text-gray-600">Dinner</p>
-          </div>
+          {sortedRecipes.slice(0, 4).map((recipe) => (
+            <div key={recipe.category_id}>
+              <Image
+                width={400}
+                height={100}
+                src={`/assets/thumbs/${recipe.thumbnail}`}
+                alt="Strawberry Cream"
+                className="w-full h-[300px] object-cover rounded-lg mb-4"
+              />
+              <h3 className="text-lg font-semibold mb-2">{recipe.title}</h3>
+              <p className="text-gray-600">{recipe.categoryName}</p>
+            </div>
+          ))}
         </div>
       </section>
     </>
